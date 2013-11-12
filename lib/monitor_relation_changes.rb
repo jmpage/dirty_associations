188162b580b275cbbca47e283964ae4ba49452e8
Jennifer Page <jenipage1989@gmail.com>
2013-11-11 23:14:17 -0500

    Initial commit

---
+++ lib/monitor_relation_changes.rb
@@ -0,0 +1,22 @@
+module MonitorRelationChanges
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    def monitor_relation_changes(field)
+      [field, "#{field.to_s.singularize}_ids"].each do |name|
+        define_method "#{name}=" do |value|
+          attribute_will_change!(field)
+          super(value)
+        end
+
+        define_method "#{name}_changed?" do
+          changed.include?(field)
+        end
+
+        define_method "#{name}_previously_changed?" do
+          previous_changes.keys.include?(field.to_s)
+        end
+      end
+    end
+  end
+end
---
+++ lib/tasks/monitor_relation_changes_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :monitor_relation_changes do
+#   # Task goes here
+# end
